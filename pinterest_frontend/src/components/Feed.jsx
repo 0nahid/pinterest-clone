87744241066ea1f50d769dedd614d42564916624
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-29 01:46:40 +0600

    added some changes on feed & sidebar

--- pinterest_frontend/src/components/Feed.jsx
+++ pinterest_frontend/src/components/Feed.jsx
@@ -1,34 +1,37 @@
-import { useState , useEffect } from "react"
-import { useParams } from "react-router-dom"
-import { client } from "../client"
-import { feedQuery, searchQuery } from "../utils/data"
-import MasonryLayout from "./MasonryLayout.jsx"
-import Spinner from "./Spinner.jsx"
-export default function Feed() {
-    const [loading, setLoading] = useState(true)
-    const [pins, setPins] = useState(null)
-    const { categoryId } = useParams();
-    useEffect(() => {
-        if (categoryId) {
-          setLoading(true);
-          const query = searchQuery(categoryId);
-          client.fetch(query).then((data) => {
-            setPins(data);
-            setLoading(false);
-          });
-        } else {
-          setLoading(true);
-          client.fetch(feedQuery).then((data) => {
-            setPins(data);
-            setLoading(false);
-          });
-        }
-      }, [categoryId]);
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
-    if(loading) return <Spinner message="We are adding new ideas to your feed!" />
+export default function Feed() {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || "new";
+  if (loading) {
     return (
-        <div>
-            {pins && <MasonryLayout pins={pins} />}
-        </div>
-    )
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 }
--- pinterest_frontend/src/components/Sidebar.jsx
+++ pinterest_frontend/src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ export default function Sidebar({ closeToggle, user }) {
           className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
           onClick={handleCloseSidebar}
         >
-          <img src={logo} alt="logo" className="w-1/2" />
+          <img src={logo} alt="logo" className="w-1/3" />
         </Link>
         <div className="flex flex-col gap-5">
           <NavLink
