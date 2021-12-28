9bb4450a57c94d45f4ec6f3f843b20b763cf950b
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-29 01:51:43 +0600

    add search

--- pinterest_frontend/src/components/Search.jsx
+++ pinterest_frontend/src/components/Search.jsx
@@ -1,7 +1,35 @@
-export default function Search() {
-    return (
-        <div>
-            Search Here
-        </div>
-    )
+import React, { useEffect, useState } from "react";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+export default function Search({ searchTerm }) {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+  return (
+    <div>
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+      )}
+    </div>
+  );
 }
