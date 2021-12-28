d776768dd99dd79b753d6c282d514939c804d388
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-29 02:33:20 +0600

    redirect to login if no user found

--- pinterest_frontend/src/App.jsx
+++ pinterest_frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import { Route, Routes } from "react-router";
-import Login from "./components/Login";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Login } from "./components";
 import Home from "./container/Home";
+
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
+  }, []);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
